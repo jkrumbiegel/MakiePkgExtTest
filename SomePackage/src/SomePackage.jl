6797756f82764b3f1f6a7a50312a98bfe7467848
Julius Krumbiegel <julius.krumbiegel@gmail.com>
2023-05-19 22:05:40 +0200

    implement error hint

--- SomePackage/src/SomePackage.jl
+++ SomePackage/src/SomePackage.jl
@@ -1,7 +1,7 @@
 module SomePackage
 
 export SomeVector
-export someplot, someplot!
+export someplot, someplot!, someotherplot, someotherplot!
 
 struct SomeVector
     v::Vector{Float32}
@@ -9,5 +9,18 @@ end
 
 function someplot end
 function someplot! end
+
+function someotherplot end
+function someotherplot! end
+
+function __init__()
+    Base.Experimental.register_error_hint(MethodError) do io, exc, argtypes, kwargs
+        if exc.f in [someplot, someplot!, someotherplot, someotherplot!]
+            if isempty(methods(exc.f))
+                print(io, "\n$(exc.f) has no methods, yet. Makie has to be loaded for the plotting extension to be activated. Run `using Makie`, `using CairoMakie`, `using GLMakie` or any other package that also loads Makie.")
+            end
+        end
+    end
+end
 
 end # module SomePackage
